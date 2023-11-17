da3dafae956a6a2d7e362c1d103da24b6c0d439f
jwc-utd <147893121+jwc-utd@users.noreply.github.com>
2023-11-17 17:22:19 -0600

    Update form.js
    add module.export

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 function checkValidInput(fname, lname, eventName, numTickets) {
-    return fname.length > 0 && lname.length > 0 && isNaN(Number(numTickets)) && Number(numTickets) > 0 && eventName != "";
+    return fname.length > 0 && lname.length > 0 && !isNaN(Number(numTickets)) && Number(numTickets) > 0 && eventName != "";
 }
 
 form = document.getElementById('form')
@@ -13,10 +13,11 @@ form.addEventListener('submit', (e) => {
     const numTickets = document.getElementById('numTickets').value;
 
     if (!validateForm(fname, lname, eventName, numTickets)) {
-        e.preventDefault(); // Prevent form submission if validation fails
+        event.preventDefault(); // Prevent form submission if validation fails
         alert('First name and last name must be at least one character long, Number of Tickets must be greater than 0 and an event must be selected')
     }
     else {
     alert('Form submitted successfully')
     }
-}) ;
+});
+module.exports = checkValidInput;
