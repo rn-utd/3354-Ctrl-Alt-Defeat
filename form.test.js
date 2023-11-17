78f2964ac68dc62eef5aa41cf4c17ac86b833098
jwc-utd <147893121+jwc-utd@users.noreply.github.com>
2023-11-17 17:22:43 -0600

    Update form.test.js
    change testing as DOM

--- form.test.js
+++ form.test.js
@@ -1,11 +1,30 @@
+
+// form.test.js
+
+const { JSDOM } = require('jsdom');
+
+// Set up a basic DOM environment
+const dom = new JSDOM('<!doctype html><html><body><form id="form"></form></body></html>');
+global.document = dom.window.document;
+global.window = dom.window;
+
+// Your actual test code follows...
+
 const checkValidInput = require('./form');
 
 test('Using invalid input', () => {
-    const isInputValid = checkInputValid('Tony', 'Stark', '', 3);
-    expect(isInputValid).toBe(False);
+    const isInputValid = checkValidInput('Tony', 'Stark', '', 3);
+    expect(isInputValid).toBe(false);
 });
 
 test('Using valid input', () => {
-    const isInputValid = checkInputValid('Tony', 'Stark', 'November 20th in Phoenix, AZ', 3);
-    expect(isInputValid).toBe(True);
+    const isInputValid = checkValidInput('Tony', 'Stark', 'November 20th in Phoenix, AZ', 3);
+    expect(isInputValid).toBe(true);
 });
+
+// Clean up after the tests
+afterAll(() => {
+  global.document = undefined;
+  global.window = undefined;
+});
+
